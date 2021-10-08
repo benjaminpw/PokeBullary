4d7cc495f57ec4aef93cafbd8570ba55fc2640bc
Benyamin Purbowasito <benyaminpurbowasito@gmail.com>
2021-10-08 08:42:26 +0700

    Initial Commit

---
+++ PokeBularry V.1.1/PokeBularry_V_1_1App.swift
@@ -0,0 +1,17 @@
+//
+//  PokeBularry_V_1_1App.swift
+//  PokeBularry V.1.1
+//
+//  Created by Benjamin Purbowasito on 08/10/21.
+//
+
+import SwiftUI
+
+@main
+struct PokeBularry_V_1_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
